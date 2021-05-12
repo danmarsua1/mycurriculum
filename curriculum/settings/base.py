4207d2335b75320f71549808b21792519e6ba7a7
Daniel J. Martínez Suárez <djmartinezsuarez91@gmail.com>
2021-05-12 22:01:51 +0200

    Add config

--- curriculum/settings/base.py
+++ curriculum/settings/base.py
@@ -88,10 +88,21 @@ WSGI_APPLICATION = 'curriculum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'curriculum',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
