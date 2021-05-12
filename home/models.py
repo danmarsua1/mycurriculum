3252d6504967ad789e4387b8272c26c5a8d41675
Daniel J. Martínez Suárez <djmartinezsuarez91@gmail.com>
2021-05-12 22:04:05 +0200

    Add starter home page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
 
 from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    pass
+
+    template = "home/home_page.html"
+
+    class Meta:
+        verbose_name = "Curriculum"
+
+
